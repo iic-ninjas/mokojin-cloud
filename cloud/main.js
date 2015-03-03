da8f9aaa52a5cf131ab02b9876afd71b00e3aec3
yon <Lighthawky@gmail.com>
2015-03-03 12:00:23 +0200

    removed images and added cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,37 @@
+var Person = Parse.Object.extend("Person", {
+  // Instance methods
+}, {
+  // Class methods
+  findOrCreate: function(name) {
+    var q = new Parse.Query(Person);
+    q.equalTo("name", name);
+    var promise = q.first().then(
+      function(user){
+        if (!user){
+          u = new Person();
+          u.set('name', name);
+          return u.save()
+        }
+      }
+    )
+    return promise;
+  }
+});
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+// Expects to receive params.name
+Parse.Cloud.define("createPerson", function(request, response) {
+  var username = request.params.name;
+  if (!username) {
+    response.error("name is required")
+    return;
+  }
+  Person.findOrCreate(username.trim()).done(
+    function(user){
+      response.success(user);
+    }
+  ).fail(
+    function (user, err){
+      response.error(err);
+    }
+  )
 });
