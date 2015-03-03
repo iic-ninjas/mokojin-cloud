d5e15b75e8a9287fad82b6f7067792dbb05ac7c4
yon <Lighthawky@gmail.com>
2015-03-03 15:17:17 +0200

    end match writter

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,4 @@
+_ = require('underscore');
 var Character = Parse.Object.extend("Character", {
 
 }, {
@@ -17,15 +18,75 @@ var Player = Parse.Object.extend("Player", {
 });
 
 
+MatchEnums = ['playerA', 'playerB', 'tie'];
 var Match = Parse.Object.extend("Match", {
-
+  isCurrent: function(){
+    return this.get('endedAt') == null;
+  },
+  endMatch: function(winner){
+    this.set('winner', winner);
+    this.set('endedAt', Date.now());
+    return this.save();
+  },
+  winner: function(){
+    var winner = this.get('winner');
+    if (winner == 'playerA'){
+      return this.get('playerA');
+    } else if (winner == 'playerB'){
+      return this.get('playerB');
+    } else {
+      return null;
+    }
+  }
+  loser: function(){
+    var winner = this.get('winner');
+    if (winner == 'playerA'){
+      return this.get('playerB');
+    } else if (winner == 'playerB'){
+      return this.get('playerA');
+    } else {
+      return null;
+    }
+  }
 }, {
+  startMatch: function(seededPlayer){
+    return Parse.Promise.when( // Get the first two QueueItems
+      [Queue.peek(0), Queue.peek(1)]
+    ).then(function(queueItem0, queueItem1){ // Make sure we have 2 players, dequeue the ones that we need from the queue
+      if (!queueItem0) return Parse.Promise.error("can't start a match - not enough players")
+      if (!queueItem1 && !seededPlayer) return Parse.Promise.error("can't start a match - not enough players")
+      return Parse.Promise.when([
+        queueItem0.dequeue(),
+        seededPlayer ? Parse.Promise.as(seededPlayer) : queueItem1.dequeue()
+      ]);
+    }).then(function(player0, player1){ // Create the new match obj
+      var match = new Match();
+      match.set('playerA', player0);
+      match.set('playerB', player1);
+      return match.save();
+    })
+  },
+  validWinnerValue: function(winner){
+    return MatchEnums.indexOf(winner) > -1
+  }
   currentMatch: function(){
     var query = new Parse.Query(Match);
     query.doesNotExist('endedAt');
     query.include('playerA');
     query.include('playerB');
+    query.include('playerA.person');
+    query.include('playerA.characterA');
+    query.include('playerA.characterB');
+    query.include('playerB.person');
+    query.include('playerB.characterA');
+    query.include('playerB.characterB');
     return query.first();
+  },
+  find: function(id){
+    var q = new Parse.Query(Match);
+    q.include('playerA');
+    q.include('playerB');
+    return q.get(id);
   },
   findPlayerInCurrentMatch: function(person){
     var promise = new Parse.Promise();
@@ -49,7 +110,12 @@ var Match = Parse.Object.extend("Match", {
 });
 
 var Queue = Parse.Object.extend("QueueItem", {
-
+  dequeue: function(){
+    var player = this.get('player');
+    return this.destroy().then(function(){
+      return player;
+    })
+  }
 }, {
   findPlayerInQueue: function(person){
     var innerQuery = new Parse.Query(Player);
@@ -65,8 +131,19 @@ var Queue = Parse.Object.extend("QueueItem", {
     queue.set('player', player);
     //TODO: might need to trigger match
     return queue.save().then(function(queueItem){
-      return Parse.Promise.as(player);
+      return player;
     });
+  },
+  peek: function(skip){
+    if (!skip) skip = 0
+    var query = new Parse.Query(Queue);
+    query.include('player');
+    query.include('player.person');
+    query.include('player.characterA');
+    query.include('player.characterB');
+    query.ascending("createdAt");
+    query.skip(skip);
+    return query.first();
   }
 });
 
@@ -142,7 +219,18 @@ Parse.Cloud.define("joinQueue", function(request, response) {
         person.joinQueue().done(
           function(player){
             if (player){
-              response.success(player);
+              Match.currentMatch().then(
+                function(match){
+                  if (match) {
+                    response.success(player);
+                    return null;
+                  } else {
+                    return Match.startMatch()
+                  }
+                }
+              ).then( function(){
+                response.success(player);
+              });
             } else {
               response.error('No player :(');
             }
@@ -163,7 +251,7 @@ Parse.Cloud.define("joinQueue", function(request, response) {
 // Returns player
 Parse.Cloud.define("setCharacter", function(request, response) {
   var playerId = request.params.player;
-  if (!player) {
+  if (!playerId) {
     response.error("player (id) is required");
     return;
   }
@@ -195,6 +283,51 @@ Parse.Cloud.define("setCharacter", function(request, response) {
           }
         )
       };
+    }
+  )
+});
+
+
+// Expects to receive params.match      [id]
+//                    params.winner     [enum]
+// Returns match
+Parse.Cloud.define("endMatch", function(request, response) {
+  var matchId = request.params.match;
+  if (!matchId) {
+    response.error("match (id) is required");
+    return;
+  }
+
+  var winner = request.params.winner;
+  if (!winner || Match.validWinnerValue(winner)) {
+    response.error("valid winner value required");
+    return;
+  }
+
+  Match.find(matchId).then(
+    function(match){
+      if (!match){
+        response.error("No match found");
+      } else if (!match.isCurrent()){
+        response.error("Can't change match after it's done");
+      }
+      return match.endMatch(winner);
+    }
+  ).then(
+    function(match){
+      var winner = match.winner();
+      var loser = match.loser();
+      loser.get('person').joinQueue().then(function(){
+        Match.startMatch(winner).then(
+          function(newMatch){
+            if (newMatch){
+              response.success(newMatch);
+            } else {
+              response.error();
+            }
+          }
+        );
+      })
     }
   )
 });
