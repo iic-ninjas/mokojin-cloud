6ff09dca881f0e1aff15dd35f314b7bf9246b65f
Udi Wertheimer <udi.wertheimer@gmail.com>
2015-03-12 16:22:39 +0200

    send push notification to invite players

--- cloud/notifications.js
+++ cloud/notifications.js
@@ -1,4 +1,6 @@
 var SESSION_DATA_CHANGED = "sessionDataChanged";
+var INVITE_PLAYERS_TITLE = "IT'S TEKKEN TIME!";
+var INVITE_PLAYERS_ALERT = "Go to the living room now!";
 
 module.exports = {
   notifySessionDataChanged: function() {
@@ -11,6 +13,18 @@ module.exports = {
       }
     }).fail(function(err) {
       console.log("error sending notification: " + err.message);
+    });
+  },
+
+  notifyInvitation: function() {
+    var everyoneQuery = new Parse.Query(Parse.Installation);
+
+    return Parse.Push.send({
+      where: new Parse.Query(Parse.Installation),
+      data: {
+        title: INVITE_PLAYERS_TITLE,
+        alert: INVITE_PLAYERS_ALERT
+      }
     });
   }
 }
